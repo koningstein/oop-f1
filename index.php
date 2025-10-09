6b4ab24bd4a41e1473cfc720771555a674097f19
Haktan-Yilmaz <9021333@student.zadkine.nl>
2025-10-09 13:27:33 +0200

    Revert "Merge branch 'develop' into driver-add"
    This reverts commit def79698ddc4b0bba0ed1cd77416dc1a6fc64e6b, reversing
    changes made to e5af28180ba5b5515901ab2b54f2515e8200d90f.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ use Smarty\Smarty;
 use Game\Driver;
 use Game\Admin;
 use Game\User;
-use Game\Lap;
 
 // 🔹 Smarty setup
 $template = new Smarty();
@@ -18,75 +17,56 @@ $page = $_GET['page'] ?? 'home';
 switch ($page) {
 
     case 'home':
-        // Actie voor het formulier
-        $template->assign('form_action', 'index.php?page=home');
-        $template->display('home.tpl');
-        break;
-
-    case 'addUser':
-        $template->display('user-create.tpl');
-        break;
-
-    case 'userProfile':
-        break;
-
-    case 'lapTimeForm':
+        // Als het formulier is ingediend
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if (!empty($_POST['sector1']) && !empty($_POST['sector2']) && !empty($_POST['sector3'])) {
+            if (!empty($_POST['sector1']) && !empty($_POST['sector2']) &&
+                !empty($_POST['sector3']) && !empty($_POST['laptime'])) {
 
                 $sector1 = (float) $_POST['sector1'];
                 $sector2 = (float) $_POST['sector2'];
                 $sector3 = (float) $_POST['sector3'];
-                $totalTime = $sector1 + $sector2 + $sector3;
+                $laptime = (float) $_POST['laptime'];
 
-                // Maak Lap object aan
-                $lap = new Game\Lap(
-                    driverId: 0,
-                    sessionId: 0,
-                    sector1: $sector1,
-                    sector2: $sector2,
-                    sector3: $sector3,
-                    totalTime: $totalTime
-                );
+                // Hier kan later database logica komen
+                // Bijvoorbeeld:
+                // $stmt = $pdo->prepare("INSERT INTO laps (sector1, sector2, sector3, laptime) VALUES (?, ?, ?, ?)");
+                // $stmt->execute([$sector1, $sector2, $sector3, $laptime]);
 
-                // Zet de lap om naar array voordat we hem in de sessie stoppen
-                $lapData = [
-                    'sector1' => $lap->getSector1(),
-                    'sector2' => $lap->getSector2(),
-                    'sector3' => $lap->getSector3(),
-                    'totalTime' => $lap->getTotalTime()
-                ];
-
-                if (!isset($_SESSION['laps'])) {
-                    $_SESSION['laps'] = [];
-                }
-                $_SESSION['laps'][] = $lapData;
-
-                $template->assign('lap', $lapData);
-                $template->assign('laps', $_SESSION['laps']);
+                // Bevestiging en data aan Smarty geven
                 $template->assign('confirmation', 'Ronde succesvol toegevoegd!');
+                $template->assign('sector1', $sector1);
+                $template->assign('sector2', $sector2);
+                $template->assign('sector3', $sector3);
+                $template->assign('laptime', $laptime);
             } else {
-                $template->assign('error', '⚠️ Vul alle sectoren in!');
+                $template->assign('error', '⚠️ Vul alle velden in!');
             }
         }
 
-        $template->assign('form_action', 'index.php?page=lapTimeForm');
-        $template->display('lap-form.tpl');
+        // Actie voor het formulier
+        $template->assign('form_action', 'index.php?page=home');
+        $template->display('home.tpl');
         break;
+  case 'addUser':
+    $template->display('user-create.tpl');
 
-    case 'leaderboard':
-        $template->assign('laps', $_SESSION['laps'] ?? []);
-        $template->display('leaderboard.tpl');
-        break;
+    break;
+  case 'userProfile':
 
-    case 'deleteLap':
-        if (isset($_POST['lapIndex']) && isset($_SESSION['laps'][$_POST['lapIndex']])) {
-            unset($_SESSION['laps'][$_POST['lapIndex']]);
-            $_SESSION['laps'] = array_values($_SESSION['laps']); // herindexeren
-        }
-        $template->assign('laps', $_SESSION['laps'] ?? []);
-        $template->display('leaderboard.tpl');
-        break;
+    break;
+
+
+
+    // 🔸 Later kun je hier meer pagina’s toevoegen:
+    // case 'leaderboard':
+    //     // Haal rondes op uit database of sessie
+    //     $template->assign('laps', $laps);
+    //     $template->display('leaderboard.tpl');
+    //     break;
+
+    // case 'about':
+    //     $template->display('about.tpl');
+    //     break;
 
     default:
         $template->display('home.tpl');
